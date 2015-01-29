b013ba481c98eedadb56f71464ac677f1e201cf7
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-01-29 17:33:54 +0000

    Started work on menu module

---
+++ engine/modules/menu/config.lua
@@ -0,0 +1,14 @@
+config = {}
+
+	config.title = "Untitled Game"
+	config.titleimage = ""
+	config.titletype = "text"
+
+	config.bgimage = "data/images/configbg.png"
+	config.options = {"Start", "Options", "Customize", "Quit"}
+
+	config.type = "scrolling_tiled"
+	config.scrolldirection = "left"
+	config.scrollspeed = 50
+
+return config
--- engine/modules/menu/init.lua
+++ engine/modules/menu/init.lua
@@ -1,10 +1,16 @@
 menu = {}
 
+local path = ... .. "."
+
+menu.config = require(path .. 'config') -- Load menu config.
+
 function menu.draw()
 
 end
 
 function menu.update(dt)
+
+print(menu.config.title)
 
 end
 
