f408623e640eab4a3ed56a302cca27adf55e83ae
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-02-16 01:21:55 +0000

    Changing conf.lua
    Still need to do things

--- src/conf.lua
+++ src/conf.lua
@@ -1,61 +1,12 @@
--- conf.lua
--- use this in place of your games normal conf.lua to see how it deals
--- with some common, or likely, limitations of uLove-targeted platforms.
---
--- Bare in mind, this is by no means a complete test, but its a minimal 
--- benchmark; if it can't run under this, then you are definitely going to
--- have problems.
-
 io.stdout:setvbuf("no") -- Prints to SublimeText's console
 
-function love.conf(t)
-    t.title             = "VideahEngine"                     -- The title of the window the game is in (string)
-    t.author            = "Ruairidh 'VideahGams' Carmichael <ruairidhcarmichael@live.co.uk>"   -- The author of the game (string)
-    t.url               = nil                               -- The website of the game (string)
+function love.conf(c)
 
-    t.identity          = nil                               -- The name of the save directory (string)
-    t.version           = "0.9.0"                           -- The LÖVE version this game was made for (string)
-    t.console           = false                             -- Attach a console (boolean, Windows only)
-    t.release           = false                             -- Enable release mode (boolean)
+	c.title = "VideahEngine"
+	c.author = "Ruairidh 'VideahGams' Carmichael"
+	c.identity = "VideahEngine"
 
+	c.window.width = 1280
+	c.window.height = 720
 
-    --t.window.icon       = "data/images/engineicon.png"
-    t.window.width      = 1280                               -- The window width (number)
-    t.window.height     = 720                              -- The window height (number)
-    t.window.fullscreen = false                             -- Enable fullscreen (boolean)
-    t.window.resizable         = true
-                                                                -- this is likely to be true
-                                                                -- on an uLove target, but few modern systems
-                                                                -- actually support this res ...so, yeah.
-
-
-    t.window.vsync      = true                              -- Enable vertical sync (boolean)
-                                                                -- actually could go either way on a uLove
-                                                                -- platform, but anything to bring your
-                                                                -- FPS down, lest you forget that most 
-                                                                -- uLove targets are likely to be sub-500MHz
-    t.window.fsaa = 0                                       -- The number of FSAA-buffers (number)
-
-
-    t.modules.joystick  = true                              -- Enable the joystick module (boolean)
-    t.modules.mouse     = true                             -- Enable the mouse module (boolean)
-                                                                -- there are far more portables without
-                                                                -- mouse-like input, than those with.
-    t.modules.keyboard  = true                              -- Enable the keyboard module (boolean)
-                                                                --  if you actually have a game pad, you 
-                                                                -- should make sure your game can be played
-                                                                -- *entirely* using that that. and no cheating with
-                                                                -- "pro" gamepads; we're talking one D-pad, A, B,
-                                                                -- R, L, Start, and Select. and that's generous.
-
-
-    t.modules.audio     = true                              -- Enable the audio module (boolean)
-    t.modules.event     = true                              -- Enable the event module (boolean)
-    t.modules.image     = true                              -- Enable the image module (boolean)
-    t.modules.graphics  = true                              -- Enable the graphics module (boolean)
-    t.modules.timer     = true                              -- Enable the timer module (boolean)
-    t.modules.sound     = true                              -- Enable the sound module (boolean)
-    t.modules.physics   = true                             -- Enable the physics module (boolean)
-                                                                -- aw, you thought uLove platforms were powerful
-                                                                -- enough to run physics, that's cute ^_^
 end
