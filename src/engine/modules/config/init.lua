6d612d79adfc44eb267a3149bdcb9b74b6cb990d
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-03-07 22:48:52 +0000

    Added basic config moduel functionality

--- src/engine/modules/config/init.lua
+++ src/engine/modules/config/init.lua
@@ -1,3 +1,16 @@
-config = {}
+local config = {}
+local confighandler = require(engine.path .. 'utils.LIP')
+
+function config.load(file)
+
+	local data = confighandler.load(file)
+	return data
+end
+
+function config.save(name, tbl)
+
+	confighandler.save(name, tbl)
+
+end
 
 return config
